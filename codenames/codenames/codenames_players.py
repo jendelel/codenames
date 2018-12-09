66d85eb939582c95531f5580400ddb88fb5d61bf
Vignesh <vsomnath@student.ethz.ch>
2018-12-09 23:00:03 +0100

    Name and function changes

--- codenames/codenames/codenames_players.py
+++ codenames/codenames/codenames_players.py
@@ -1,27 +1,29 @@
 import numpy as np
 
+# TODO: Add classes for spymaster and guessers
 
-class RandomCodenamesPlayer():
+
+class RandomCodenamesPlayer:
 
     def __init__(self, game):
         self.game = game
 
     def play(self, board):
-        a = np.random.randint(self.game.getActionSize())
-        valids = self.game.getValidMoves(board, 1)
+        a = np.random.randint(self.game.get_action_size())
+        valids = self.game.get_valid_moves(board, 1)
         while valids[a] != 1:
-            a = np.random.randint(self.game.getActionSize())
+            a = np.random.randint(self.game.get_action_size())
         return a
 
 
-class HumanCodenamesPlayer():
+class HumanCodenamesPlayer:
 
     def __init__(self, game):
         self.game = game
 
     def play(self, board):
         # display(board)
-        valid = self.game.getValidMoves(board, 1)
+        valid = self.game.get_valid_moves(board, 1)
         for i in range(len(valid)):
             if valid[i]:
                 print(int(i / self.game.n), int(i % self.game.n))
@@ -38,19 +40,19 @@ class HumanCodenamesPlayer():
         return a
 
 
-class GreedyCodenamesPlayer():
+class GreedyCodenamesPlayer:
 
     def __init__(self, game):
         self.game = game
 
     def play(self, board):
-        valids = self.game.getValidMoves(board, 1)
+        valids = self.game.get_valid_moves(board, 1)
         candidates = []
-        for a in range(self.game.getActionSize()):
+        for a in range(self.game.get_action_size()):
             if valids[a] == 0:
                 continue
-            nextBoard, _ = self.game.getNextState(board, 1, a)
-            score = self.game.getScore(nextBoard, 1)
+            next_board, _ = self.game.get_next_state(board, 1, a)
+            score = self.game.get_score(next_board, 1)
             candidates += [(-score, a)]
         candidates.sort()
         return candidates[0][1]
